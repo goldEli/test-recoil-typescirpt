f3b3ea81d7336fb952d093ce0c96edb0ea1998cd
缪宇 <1337611267@qq.com>
2021-01-27 16:30:57 +0800

    use selector

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
-import { todoListState } from "../../store";
+import { todoListState, filteredTodoListState } from "../../store";
 import TodoItemCreator from "../TodoItemCreator";
 import TodoItem from "../TodoItem";
+import TodoListStats from "../TodoListStats";
+import TodoListFilters from "../TodoListFilters";
 
 export default function TodoList() {
-  const todoList = useRecoilValue(todoListState);
+  // const todoList = useRecoilValue(todoListState);
+  // changed from todoListState to filteredTodoListState
+  const todoList = useRecoilValue(filteredTodoListState);
 
   return (
     <>
       <h1>to do list</h1>
-      {/* <TodoListStats /> */}
-      {/* <TodoListFilters /> */}
+      <TodoListStats />
+      <TodoListFilters />
       <TodoItemCreator />
 
       {todoList.map((todoItem) => (
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface ITodoListState {
   isComplete: boolean;
@@ -6,9 +6,50 @@ export interface ITodoListState {
   text: string;
 }
 
-const initTodoListState: ITodoListState[] = [];
-
-export const todoListState = atom({
+export const todoListState = atom<ITodoListState[]>({
   key: "todoListState",
-  default: initTodoListState
+  default: []
+});
+
+export const todoListFilterState = atom<
+  "Show All" | "Show Completed" | "Show Uncompleted"
+>({
+  key: "todoListFilterState",
+  default: "Show All"
+});
+
+export const filteredTodoListState = selector({
+  key: "filteredTodoListState",
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+
+    switch (filter) {
+      case "Show Completed":
+        return list.filter((item) => item.isComplete);
+      case "Show Uncompleted":
+        return list.filter((item) => !item.isComplete);
+      default:
+        return list;
+    }
+  }
+});
+
+export const todoListStatsState = selector({
+  key: "todoListStatsState",
+  get: ({ get }) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted =
+      totalNum === 0 ? 0 : (totalCompletedNum / totalNum) * 100;
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted
+    };
+  }
 });
